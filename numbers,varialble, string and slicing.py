e23a65e26df4a40187bbdd51366b0c3ae566c30a
delobos <54415469+delobos@users.noreply.github.com>
2019-09-02 21:03:16 -0400

    Add files via upload
    This is the Jupyter Notebook showing me following along and doing exercises on numbers, assigning variables, and strings & slicing.

---
+++ numbers,varialble, string and slicing.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a =10
+
+
+# In[ ]:
+
+
+a
+
+
+# In[ ]:
+
+
+a = a + a
+
+
+# In[ ]:
+
+
+a
+
+
+# In[ ]:
+
+
+type(a)
+
+
+# In[26]:
+
+
+a = 30.1
+
+
+# In[ ]:
+
+
+type(a)
+
+
+# In[ ]:
+
+
+int = 4
+
+
+# In[ ]:
+
+
+int
+
+
+# In[1]:
+
+
+int
+
+
+# In[27]:
+
+
+a
+
+
+# In[28]:
+
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+
+
+# In[6]:
+
+
+'hello'
+
+
+# In[7]:
+
+
+"world"
+
+
+# In[8]:
+
+
+'this is also a string'
+
+
+# In[9]:
+
+
+"I'm going on a run"
+
+
+# In[10]:
+
+
+print("hello")
+
+
+# In[12]:
+
+
+print("hello \tworld")
+
+
+# In[13]:
+
+
+len("hello")
+
+
+# In[14]:
+
+
+mystring = "Hello World"
+
+
+# In[15]:
+
+
+mystring
+
+
+# In[16]:
+
+
+mystring[0]
+
+
+# In[17]:
+
+
+mystring[8]
+
+
+# In[18]:
+
+
+mystring[-2]
+
+
+# In[20]:
+
+
+mystring[-3]
+
+
+# In[21]:
+
+
+mystring = "abcdefghijk"
+
+
+# In[22]:
+
+
+mystring
+
+
+# In[23]:
+
+
+mystring[2:]
+
+
+# In[24]:
+
+
+mystring[:3]
+
+
+# In[30]:
+
+
+mystring[3:6]
+
+
+# In[31]:
+
+
+mystring[1:3]
+
+
+# In[32]:
+
+
+mystring[::2]
+
+
+# In[33]:
+
+
+mystring[::3]
+
+
+# In[34]:
+
+
+mystring[2:7:2]
+
+
+# In[37]:
+
+
+mystring[::-1]
+
+
+# In[ ]:
+
+
+
+
